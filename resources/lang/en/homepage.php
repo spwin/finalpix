84972c5481db021269cb8181b8d932a918ba1aa0
Stanislav <stanislav@pixsens.co.uk>
2016-12-02 06:32:51 +0000

    homepage phrase changed

--- resources/lang/en/homepage.php
+++ resources/lang/en/homepage.php
@@ -3,5 +3,5 @@
 return [
     'choose-language' => '<span class="green">Choose</span> your <span class="green">language</span>',
     'our-services' => 'Our <span class="green">services</span>',
-    'home-title' => 'WEB design, development<br> and SEO services in London',
+    'home-title' => 'WEB design, development<br> and SEO services London',
 ];
